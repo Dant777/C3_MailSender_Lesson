78769006f5c2dd0e07ff179cf7a5c8471c8196bf
Dant777 <kolbt87@gmail.com>
2019-06-21 00:41:12 +0300

    Планировщик

--- MailSender_practis/MailSender_practis/MainWindow.xaml.cs
+++ MailSender_practis/MailSender_practis/MainWindow.xaml.cs
@@ -14,11 +14,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace MailSender_practis
-{
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+{ 
+    public partial class MainWindow
     {
         public MainWindow()
         {
