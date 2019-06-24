a8420d81f62172f7665c9caf01c25b3b650843f8
Dant777 <kolbt87@gmail.com>
2019-06-24 21:24:47 +0300

    Подключение базы данных

---
+++ MailSender_practis/MailSender_practis/Components/TabConroler.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MailSender_practis.Components
+{
+    /// <summary>
+    /// Логика взаимодействия для TabConroler.xaml
+    /// </summary>
+    public partial class TabConroler : UserControl
+    {
+        public TabConroler() =>InitializeComponent();
+
+        public bool IsLeftButtonVisible
+        {
+            get => MoveLeft.Visibility == Visibility.Visible;
+            set => MoveLeft.Visibility = value ? Visibility.Visible : Visibility.Collapsed;
+        }
+        public bool IsRightButtonVisible
+        {
+            get => MoveRight.Visibility == Visibility.Visible;
+            set => MoveRight.Visibility = value ? Visibility.Visible : Visibility.Collapsed;
+        }
+        public event EventHandler LeftButtonClick;
+        public event EventHandler RightButtonClick;
+        public void OnButtonClick(object Sender, RoutedEventArgs E)
+        {
+            if (!(Sender is Button button)) return;
+            switch (button.Name)
+            {
+                case "MoveLeft":
+                    LeftButtonClick?.Invoke(this, EventArgs.Empty);
+                    break;
+                case "MoveRight":
+                    RightButtonClick?.Invoke(this, EventArgs.Empty);
+                    break;
+                
+            }
+        }
+    }
+}
--- MailSender_practis/MailSender_practis/MainWindow.xaml.cs
+++ MailSender_practis/MailSender_practis/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MailSender_practis.Components;
 
 namespace MailSender_practis
 { 
@@ -25,6 +26,26 @@ namespace MailSender_practis
         private void Exit_Click(object sender, RoutedEventArgs e)
         {
             Close();
+        }
+
+        private void TabConroler_LeftButtonClick(object Sender, EventArgs e)
+        {
+            if (!(Sender is TabConroler tab_controller)) return;
+            if (tab_controller.IsLeftButtonVisible)
+                 MainTabControle.SelectedIndex--;
+            tab_controller.IsLeftButtonVisible = MainTabControle.SelectedIndex > 0;
+            tab_controller.IsRightButtonVisible = MainTabControle.SelectedIndex < MainTabControle.Items.Count;
+        }
+
+        private void TabConroler_RightButtonClick(object Sender, EventArgs e)
+        {
+            if (!(Sender is TabConroler tab_controller)) return;
+            if (tab_controller.IsRightButtonVisible)
+                MainTabControle.SelectedIndex++;
+
+            tab_controller.IsLeftButtonVisible = MainTabControle.SelectedIndex > 0;
+            tab_controller.IsRightButtonVisible = MainTabControle.SelectedIndex < MainTabControle.Items.Count - 1;
+
         }
     }
 }
