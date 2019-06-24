810b283fd9c8449a0a7088383c7d63a47c7a38d7
Dant777 <kolbt87@gmail.com>
2019-06-24 21:53:22 +0300

    Создание доп класов: 1. класс для пароля и отправителя

---
+++ MailSender_practis/MailSender_Lib/Data/VariablesClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailSender_Lib.Data
+{
+    public static class VariablesClass
+    {
+        public static Dictionary<string, string> Senders
+        {
+            get { return dicSenders; }
+        }
+        private static Dictionary<string, string> dicSenders = new Dictionary<string, string>()
+        {
+            { "vl.kolbt87@yandex.ru","ibnjrfy8787" },
+            { "sok74@yandex.ru",PasswordClass.getPassword(";liq34tjk") }
+        };
+    }
+}
